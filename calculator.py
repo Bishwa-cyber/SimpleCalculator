eb6aa022855ccb681c7b482aef1bb1d93c6bf84d
Bishwa-cyber <159514333+Bishwa-cyber@users.noreply.github.com>
2024-03-09 22:09:35 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,8 @@
+
+a =10
+b = 20
+print("the value of ",a,"+",3,"is",a+b)
+print("the value of ",a,"-",3,"is",a-b)
+print("the value of ",a,"/",3,"is",a/b)
+print("the value of ",a,"*",3,"is",a*b)
+print("the value of ",a,"//",3,"is",a//b)
